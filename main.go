05fcd48106375c26ce76f6746b36ff3591e8b0a5
Evgen Vasilyev <evgen.vasilyev@gmail.com>
2018-05-25 14:36:49 +0200

    Telegram bot connected.

--- main.go
+++ main.go
@@ -1,6 +1,46 @@
 package main
 
+import (
+	"flag"
+	"io/ioutil"
+	"encoding/json"
+	"gopkg.in/telegram-bot-api.v4"
+)
+
+type Config struct {
+	Token string `json:"token"`
+	Chat int64 `json:"chat"`
+}
+
 func main() {
-	/*Connect to the telegram bot*/
+	var (
+		configFile string
+	)
+
+	flag.StringVar(&configFile, "config", "config.json", "Configuration file path")
+	flag.Parse()
+
+	file, err := ioutil.ReadFile(configFile)
+	if err != nil {
+		panic(err)
+	}
+
+	var config Config
+	err = json.Unmarshal(file, &config)
+	if err != nil {
+		panic(err)
+	}
+
+	telebot, err := tgbotapi.NewBotAPI(config.Token)
+	if err != nil {
+		panic(err)
+	}
+
+	msg := tgbotapi.NewMessage(config.Chat, "Bot started")
+	_, err = telebot.Send(msg)
+	if err != nil {
+		panic(err)
+	}
+
 	/*Program the telegram bot*/
 }
